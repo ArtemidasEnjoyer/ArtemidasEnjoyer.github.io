8df8cd64538827c903f97bda64566ad60da3e518
Artemida <121769414+ArtemidasEnjoyer@users.noreply.github.com>
2025-10-31 09:27:18 +0100

    Add files via upload

---
+++ e5/szkolenie.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Firma szkoleniowa</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        effect działania skryptu
+    </section>
+    
+</body>
+</html>
